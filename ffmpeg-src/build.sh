a95f7aa7426136f572056bc2ee6f9972131921d3
Razvan Miclius <razvang0307@gmail.com>
2023-07-15 14:06:51 +0300

    app/ffmpeg-source: Add ffmpeg as a dynamic app
    Signed-off-by: Razvan Miclius <razvang0307@gmail.com>

---
+++ ffmpeg-src/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Clean up.
+rm -fr ffmpeg* app-ffmpeg
+
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
+
+echo "Configuring ffmpeg..."
+pushd ffmpeg &> /dev/null || exit 1
+./configure
+
+echo "Building ffmpeg..."
+make -j "$(nproc)"
+
+popd &> /dev/null || exit 1
+mkdir app-ffmpeg
+echo "Extracting ffmpeg..."
+
+../extract.sh ffmpeg/ffmpeg app-ffmpeg
+pushd app-ffmpeg &> /dev/null || exit 1
+cp ../ffmpeg/ffmpeg .
