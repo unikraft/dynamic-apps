1548322028781c8d6df10ed9b7c6b91b2e1d4b5a
Razvan Deaconescu <razvand@unikraft.io>
2023-09-22 15:09:43 +0300

    lang/go: Update libraries for helloworld
    Running GO helloworld resulted in a library version error. Replace
    libraries to make Go helloworld work.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- lang/go/helloworld/helloworld.go
+++ lang/go/helloworld/helloworld.go
@@ -6,5 +6,8 @@ import (
 )
 
 func main() {
+	// net.IPv4len is required to build a dynamic executable.
+	// Otherwise, the Go compiler seems unable to build one,
+	// only a static executable.
 	fmt.Printf("Hello, world (%d)!\n", net.IPv4len)
 }
