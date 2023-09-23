e08f0770236a15ac1d778f8de7ca42719631a070
Razvan Deaconescu <razvand@unikraft.io>
2023-09-23 18:40:49 +0300

    extract.sh: Simplify script
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- extract.sh
+++ extract.sh
@@ -2,10 +2,6 @@
 
 binary="$1"
 extract_path="$2"
-# By default, do not copy binary.
-# Copy binary only if command passed as argument.
-# Otherwise, assume the binary path is the destination.
-copy_binary=0
 
 if test -z "$binary"; then
     echo "Binary to extract not provided." 1>&2
@@ -20,36 +16,27 @@ if test -z "$extract_path"; then
     extract_path=$(pwd)
 fi
 
-if [[ "$binary" == *"/"* ]]; then
-    binary_path="$binary"
-else
-    binary_path=$(which "$binary")
-    copy_binary=1
-fi
-
-if test -z "$binary_path"; then
+if test -z "$binary"; then
     echo "Unknown binary $binary" 1>&2
     exit 1
 fi
 
-if test ! -f "$binary_path"; then
+if test ! -f "$binary"; then
     echo "$binary_path is not a file" 1>&2
     exit 1
 fi
 
-if ! ldd "$binary_path" > /dev/null 2>&1; then
+if ! ldd "$binary" > /dev/null 2>&1; then
     echo "$binary_path is not an ELF" 1>&2
     exit 1
 fi
 
 # First copy the ELF.
-if test "$copy_binary" -eq 1; then
-    echo "Copying $binary_path ..."
-    cp --dereference --parents "$binary_path" "$extract_path"
-fi
+echo "Copying $binary ..."
+cp --dereference --parents "$binary" "$extract_path"
 
-# Then copy dependencies
-for l in $(ldd "$binary_path" | grep -o '/[^ ]*'); do
+# Then copy the shared object dependencies.
+for l in $(ldd "$binary" | grep -o '/[^ ]*'); do
     echo "Copying $l ..."
     cp --dereference --parents "$l" "$extract_path"
 done
